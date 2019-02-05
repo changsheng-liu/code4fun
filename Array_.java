b214796f2884f3e33841545a5bbf2cf94cfe1f97
Changsheng Liu <liucs90@hotmail.com>
2019-02-04 22:17:44 -0800

    update stack and queue

--- Array_.java
+++ Array_.java
@@ -4,11 +4,44 @@ import java.util.Deque;
 class Array_ {
     void forstack(){
         Deque<Integer> s = new ArrayDeque<Integer>();
+        // for stack 
         s.push(1);
+        s.push(2);
+        System.out.println(s);
+        System.out.println(s.peek());
+        s.pop();
+        System.out.println(s.peek());
+    }
+
+    void forqueue(){
+        Deque<Integer> s = new ArrayDeque<Integer>();
+        s.offer(1);
+        s.offer(2);
+        System.out.println(s);
+        System.out.println(s.peek());
+        s.poll();
+        System.out.println(s.peek());
+    }
+
+    void forgeneral() {
+        Deque<Integer> s = new ArrayDeque<Integer>();
+        s.add(1);
+        s.add(2);
+        s.add(3);
+        System.out.println(s);
+        s.remove();
+        System.out.println(s);
+        s.removeLast();
+        System.out.println(s);
     }
 
     public static void main(String[] args) {
         Array_ a = new Array_();
+        System.out.println("stack:");
         a.forstack();
+        System.out.println("queue:");
+        a.forqueue();
+        System.out.println("general:");
+        a.forgeneral();
     }
 }
